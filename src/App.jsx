70e149df9cf74809633c55b52f83840ec3d331ab
Paul <paulmichaelhardman@gmail.com>
2022-03-07 15:18:27 +0000

    push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss';
+import Nav from './components/Nav/Nav';
+
+function App() {
+  return (
+    <div className="App">
+<h1>YOYOYOYO</h1>
+<h2>gogogo</h2>
+
+<Nav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Nav.scss";
+
+const Nav = () => {
+  return (
+
+    <h1>P DOG IN THAT HOUSE!</h1>
+  );
+};
+
+export default Nav;
