b0be42f26e41da4636d7c50ab45aac3c4e799f9b
Paul <paulmichaelhardman@gmail.com>
2022-03-09 18:31:24 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,47 @@
-import "./App.scss";
-import Filter from "./components/Filter/Filter";
-import Footer from "./components/Footer/Footer";
-import Nav from "./components/Nav/Nav";
-import Tickets from "./components/Tickets/Tickets";
-import team from "./data/team.js";
-import React, { useState } from "react";
-
-function App() {
-  // my states go here
-
-  // const setDemo = () => {
-  //   console.log("Hello Console")
-  // }
-  // const [demo, setDemo] = useState(" ");
-
-  // for Demo purpose in real use use the value below commented out
-  // const [counter, setCounter] = useState(Math.floor(Math.random() * 20));
-  const [counter, setCounter] = useState(0);
-
-  // make a copy of the data object
-  const teamCopy = [...team];
-  console.log(teamCopy);
-  team.forEach((member) => {member.counter = 0});
-  console.log(teamCopy);
+import { useState } from "react";
+import React from 'react'
 
 
-  // set our base state
 
+const App = () => {
+  const [data, setData] = useState([
+    { id: 0, name: "barry" },
+    { id: 1, name: "john" },
+    { id: 2, name: "jane" },
+  ]);
 
-const [count,  setCount] = useState([teamCopy])
+let variableValue = 4
 
-  const handleDecrement = () => {
-    if (counter < 1) {
-      setCounter(0);
-    } else {
-      setCounter(counter - 1);
-    }
-  };
-  const handleIncrement = () => {
-    setCounter(counter + 1);
+  const doSomeUpdate = () => {
+    setData(
+      data.map((element) => {
+        return {
+          id: element.id,
+          name: "heather",
+          StateNum: variableValue
+        };
+      })
+    );
   };
 
-  const colorIncrement = () => {
-    if (counter >= 0 && counter <= 5) {
-      return "teamMember green";
-    } else if (counter >= 6 && counter <= 15) {
-      return "teamMember yellow";
-    } else if (counter >= 16) {
-      return "teamMember red";
-    } else {
-      return "teamMember red";
-    }
-  };
+  const doSomeOtherUpdate = () => {
+    variableValue +=1;
+  }
+
 
   return (
-    <div className="App">
-      <div className="App-grid">
-        <Nav className="App-grid__Nav" />
-        <Filter className="App-grid__Filter" />
-        <Tickets
-          className="App-grid__Tickets"
-          teamArray={team}
-          counter={counter}
-          teamCopyWState = {teamCopy}
-          setCounter={setCounter}
-          handleDecrement={handleDecrement}
-          handleIncrement={handleIncrement}
-          colorIncrement={colorIncrement}
-          // warning={warning}
-        />
-      </div>
-      <Footer />
+    <div className="app">
+      {data.map(({ name, id, StateNum }) => {
+        return (
+          <p>
+            {name} ({id}) {StateNum} <button onClick={doSomeOtherUpdate}>TEST BUTTON</button>
+          </p>
+        );
+      })}
+      <button onClick={doSomeUpdate}>click</button>
+     
     </div>
   );
-}
+};
 
 export default App;
