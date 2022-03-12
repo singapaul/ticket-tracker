691f7dc2d2aa0f4c6d0509fbe8e4b2d38993b84f
Paul <paulmichaelhardman@gmail.com>
2022-03-12 22:10:26 +0000

    got the basics down

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Nav from "./components/Nav/Nav";
 import Tickets from "./components/Tickets/Tickets";
 import team from "./data/team.js";
 import React, { useState } from "react";
+import { getQueriesForElement } from "@testing-library/react";
 
 function App() {
   // Setting the initial app state
@@ -32,26 +33,55 @@ function App() {
 
   // function to decrement a value
   const handleDecrement = (event) => {
-    console.log(event);
-    console.log(event.target);
     setTeamData(
-      teamData.map((obj) =>
-        obj.id == event.target.id ? { ...obj, counter: obj.counter - 1 } : obj
-      )
+      teamData.map((obj) => {
+        if (obj.counter > 0) {
+          return obj.id == event.target.id
+            ? { ...obj, counter: obj.counter - 1 }
+            : obj;
+        } else {
+          return obj.id == event.target.id ? { ...obj, counter: 0 } : obj;
+        }
+      })
     );
   };
 
-  // copy and paste are please don't implement anything below this
-// copy and paste are please don't implement anything above/below this
+  // // copy and paste are please don't implement anything below this
 
 
+  // const colorIncrement = () => {
+  //   if (counter >= 0 && counter <= 5) {
+  //     return "teamMember green";
+  //   } else if (counter >= 6 && counter <= 15) {
+  //     return "teamMember yellow";
+  //   } else if (counter >= 16) {
+  //     return "teamMember red";
+  //   } else {
+  //     return "teamMember red";
+  //   }
+  // };
 
+  // const warningFlag = () => {
+  //   if (counter > 24) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // };
 
+  // const warning = () => {
+  //   if (counter <25) {
+  //     return teamMember.role
+  //   } else if (counter >= 24 && counter <=40) {
+  //     return "Get cracking!";
+  //   } else if (counter > 39) {
+  //     return "Please submit P45";
+  //   } else {
+  //     return
+  //   }
+  // };
 
-
-
-
-
+  // copy and paste are please don't implement anything above this
 
   // copy and paste are please don't implement anything above this
 
--- src/components/Tickets/Tickets.jsx
+++ src/components/Tickets/Tickets.jsx
@@ -4,14 +4,21 @@ import "./Tickets.scss";
 
 const Tickets = ({teamData, handleIncrement, handleDecrement}) => {
   const cardsArray = teamData.map((member) => {
+    console.log(member.name)
+    console.log(member.id)
+    console.log(member.counter);
+
+    
+  
+
     return (
       <div key={member.id} className="here">
         <p>{member.name}</p>
         <p>{member.role}</p>
         <p>{member.id}</p>
-        <button id={member.id} onClick={handleDecrement}>SUBTRACT</button>
+        <button id={member.id} onClick={handleDecrement}>-</button>
         <p>{member.counter}</p>
-        <button id={member.id} onClick={handleIncrement}>ADD</button>
+        <button id={member.id} onClick={handleIncrement}>+</button>
         
         </div>
     );
