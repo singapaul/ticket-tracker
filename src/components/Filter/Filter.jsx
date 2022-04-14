9647147d5fbb1c92a06d25318866f4cd383d1d2f
Paul <paulmichaelhardman@gmail.com>
2022-04-15 00:09:01 +0100

    filters fixed

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -12,26 +12,33 @@ const Filter = ({
   return (
     <div className="filter">
       <h2 className="filter__heading">Filter</h2>
-      <h4 className="filter__subHeading">By job</h4>
-      <input
-        type="text"
-        label={"input"}
-        value={jobValue}
-        onChange={handlejobSearch}
-      />
-      <h4 className="filter__subHeading">By employee</h4>
-      <input
-        type="text"
-        label={"input"}
-        value={searchValue}
-        onChange={handleSearch}
-      />
-      <h4 className="filter__subHeading">Sort dropdown</h4>
-      <select onChange={handleSort} value={selectedDropdown}>
-        <option value={"default"}>Default</option>
-        <option value={"hilow"}>Highest-Lowest</option>
-        <option value={"lowhi"}>Lowest-Highest</option>
-      </select>
+
+      <div>
+        <h4 className="filter__subHeading">By job</h4>
+        <input
+          type="text"
+          label={"input"}
+          value={jobValue}
+          onChange={handlejobSearch}
+        />
+      </div>
+      <div>
+        <h4 className="filter__subHeading">By employee</h4>
+        <input
+          type="text"
+          label={"input"}
+          value={searchValue}
+          onChange={handleSearch}
+        />
+      </div>
+      <div>
+        <h4 className="filter__subHeading">Sort dropdown</h4>
+        <select onChange={handleSort} value={selectedDropdown}>
+          <option value={"default"}>Default</option>
+          <option value={"hilow"}>Highest-Lowest</option>
+          <option value={"lowhi"}>Lowest-Highest</option>
+        </select>
+      </div>
     </div>
   );
 };
